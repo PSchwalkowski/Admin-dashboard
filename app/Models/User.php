a370b5abe7f54c68728ad4d62d27c4ef6e285be9
Paweł <p.schwalkowski@gmail.com>
2017-02-27 19:57:11 +0100

    ENTRUST
    https://github.com/Zizaco/entrust

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,8 +5,10 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable {
+use Zizaco\Entrust\Traits\EntrustUserTrait;
 
+class User extends Authenticatable {
+	use EntrustUserTrait;
   use Notifiable;
 
   /**
--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
          */
         Laravel\Tinker\TinkerServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+				Zizaco\Entrust\EntrustServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -229,6 +230,7 @@ return [
 
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+				'Entrust' => Zizaco\Entrust\EntrustFacade::class,
 
     ],
 
